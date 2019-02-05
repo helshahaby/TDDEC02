e730f93bd1162fd9908449e24d198660d72cb451
Hossam <hossam.elshahaby@gmail.com>
2019-02-05 12:37:44 +0200

    Update Tennis.c

--- Tennis.c
+++ Tennis.c
@@ -9,124 +9,137 @@ char player1Name[20]="Player 1";
 char player2Name[20]="Player 2";
 
 void InitTennisGame(){
-    player1Points=0;
-    player2Points=0;
-    memset(player1Name,0,20);
-    memset(player2Name,0,20);
+	player1Points=0;
+	player2Points=0;
+	//memset(player1Name,0,20);
+	//memset(player2Name,0,20);
 }
 
 void playerOneScores() {
-    player1Points++;
+	player1Points++;
 }
 
 void playerTwoScores() {
-    player2Points++;
+	player2Points++;
+}
+
+void UpdateScore(int playerPoints , char playerResult[20]){
+
+	if (player1Points==0)
+		strcpy(playerResult,"Love");
+
+	if (playerPoints==1)
+		strcpy(playerResult,"Fifteen");
+
+	if (playerPoints==2)
+		strcpy(playerResult,"Thirty");
+
+	if (playerPoints==3)
+		strcpy(playerResult,"Forty");
+
+}
+
+void UpdateNullScore(int player1Points,int player2Points, char player1Result[20], char player2Result[20])
+{
+	if (player1Points == 0)
+	{
+		strcpy(player1Result,"Love");
+	}
+	else if (player2Points == 0)
+	{
+		strcpy(player2Result,"Love");
+	}
+
+
+}
+
+void printScore(char score[20],char player1Result[20],char player2Result[20])
+{
+	strcpy(score,player1Result);
+	strcat(score,", ");
+	strcat(score,player2Result);
+}
+
+void UpdateAdvantageScore(int player1Points,int player2Points, char score[20])
+{
+	if (player1Points > player2Points && player2Points >= 3)
+	{
+		sprintf(score,"Advantage %s",player1Name);
+	}
+
+	if (player2Points > player1Points && player1Points >= 3)
+	{
+		sprintf(score,"Advantage %s",player2Name);
+	}
+}
+
+void UpdateWinningPlayer(int player1Points,int player2Points, char score[20])
+{
+	if (player1Points>=4 && player2Points>=0 && (player1Points-player2Points)>=2)
+	{
+		sprintf(score,"%s wins",player1Name);
+	}
+	if (player2Points>=4 && player1Points>=0 && (player2Points-player1Points)>=2)
+	{
+		sprintf(score,"%s wins",player2Name);
+	}        
+}
+
+void playerAheadOtherNull(int player1Points,int player2Points,char player1Result[20],char player2Result[20],char score[20])
+{
+
+	if ((player1Points > 0 && player2Points==0)   )
+	{
+		UpdateScore(player1Points , player1Result);
+		UpdateNullScore(player1Points, player2Points, player1Result ,player2Result);
+
+		printScore(score,player1Result,player2Result);
+	}
+	else if (player2Points > 0 && player1Points==0)
+	{
+		UpdateScore(player2Points , player2Result);
+		UpdateNullScore(player1Points, player2Points, player1Result ,player2Result);
+
+		printScore(score,player1Result,player2Result);
+	}
+
+
+}
+
+void player1AheadLessThanFourPoints(int player1Points,int player2Points,char player1Result[20],char player2Result[20],char score[20])
+{
+	if ((player1Points>player2Points && player1Points < 4) || (player2Points>player1Points && player2Points < 4)  )
+	{
+		UpdateScore(player1Points , player1Result);
+		UpdateScore(player2Points , player2Result);
+		printScore(score,player1Result,player2Result);
+
+	}
 }
 
 void getScore(char * score){
-char player1Result[32] = "";
-char player2Result[32] = "";
-    
-    if (player1Points == player2Points && player1Points < 4)
-    {
-        if (player1Points==0)
-            strcpy(score,"Love");
-        
-        if (player1Points==1)
-            strcpy(score,"Fifteen");
-        
-        if (player1Points==2)
-            strcpy(score,"Thirty");
 
-        strcat (score," all");
-    }
-    
-    if (player1Points==player2Points && player1Points>=3)
-        strcpy(score,"Deuce");
+	char player1Result[32] = "";
+	char player2Result[32] = "";
 
-    if (player1Points > 0 && player2Points==0)
-    {
-        if (player1Points==1)
-            strcpy(player1Result,"Fifteen");
+	if (player1Points == player2Points && player1Points <3 )
+	{
 
-        if (player1Points==2)
-            strcpy(player1Result,"Thirty");
+		UpdateScore(player1Points , score);
 
-        if (player1Points==3)
-            strcpy(player1Result,"Forty");
+		strcat (score," all");
+	}
 
-        strcpy(player2Result,"Love");
-        strcpy(score,player1Result);
-        strcat(score,", ");
-        strcat(score,player2Result);
-    }
-    
-    if (player2Points > 0 && player1Points==0)
-    {
-        if (player2Points==1)
-            strcpy(player2Result,"Fifteen");
+	if (player1Points==player2Points && player1Points>=3)
+		strcpy(score,"Deuce");
 
-        if (player2Points==2)
-            strcpy(player2Result,"Thirty");
+	playerAheadOtherNull( player1Points, player2Points, player1Result , player2Result,score);
 
-        if (player2Points==3)
-            strcpy(player2Result,"Forty");
+	player1AheadLessThanFourPoints(player1Points , player2Points , player1Result , player2Result , score);
 
-        strcpy(player1Result,"Love");
-        strcpy(score,player1Result);
-        strcat(score,", ");
-        strcat(score,player2Result);
-    }
+	UpdateAdvantageScore(player1Points,player2Points,score);
 
-    if (player1Points>player2Points && player1Points < 4)
-    {
-        if (player1Points==2)
-            strcpy(player1Result,"Thirty");
-        if (player1Points==3)
-            strcpy(player1Result,"Forty");
-        if (player2Points==1)
-            strcpy(player2Result,"Fifteen");
-        if (player2Points==2)
-            strcpy(player2Result,"Thirty");
+	UpdateWinningPlayer(player1Points,player2Points,score);
 
-        strcpy(score,player1Result);
-        strcat(score,", ");
-        strcat(score,player2Result);
-    }
-    
-    if (player2Points>player1Points && player2Points < 4)
-    {
-        if (player2Points==2)
-            strcpy(player2Result,"Thirty");
-        if (player2Points==3)
-            strcpy(player2Result,"Forty");
-        if (player1Points==1)
-            strcpy(player1Result,"Fifteen");
-        if (player1Points==2)
-            strcpy(player1Result,"Thirty");
-
-        strcpy(score,player1Result);
-        strcat(score,", ");
-        strcat(score,player2Result);
-    }
-
-    if (player1Points > player2Points && player2Points >= 3)
-    {
-        sprintf(score,"Advantage %s",player1Name);
-    }
-
-    if (player2Points > player1Points && player1Points >= 3)
-    {
-        sprintf(score,"Advantage %s",player2Name);
-    }
-
-    if (player1Points>=4 && player2Points>=0 && (player1Points-player2Points)>=2)
-    {
-        sprintf(score,"%s wins",player1Name);
-    }
-    if (player2Points>=4 && player1Points>=0 && (player2Points-player1Points)>=2)
-    {
-        sprintf(score,"%s wins",player2Name);
-    }
 }
 
